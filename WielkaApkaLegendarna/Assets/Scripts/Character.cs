3509c847a3cb59531f29efb5f4c957431b2e275a
Stanisław Franczak <taktycznybociandev@gmail.com>
2023-01-09 00:48:49 +0100

    Adding leyout

---
+++ WielkaApkaLegendarna/Assets/Scripts/Character.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+
+    public string imie;
+    public string koncepcja;
+
+    public int sila;
+    public int zrecznosc;
+    public int umysl;
+
+    public int wytrzymalosc;
+    public int heroizm;
+    public int atak;
+    public int obrona;
+
+    public int[] skills;
+    public int[] eq;
+
+    public string notes;
+    public string story;
+
+    public int PD;
+
+}
---
+++ WielkaApkaLegendarna/Assets/Scripts/UmiejetnosciTemplate.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Skill", menuName = "Skill")]
+public class UmiejetnosciTemplate : ScriptableObject
+{
+    //What is this skill is known from ID.
+    public int ID;
+
+    public string skillName;
+    public string description;
+
+}
